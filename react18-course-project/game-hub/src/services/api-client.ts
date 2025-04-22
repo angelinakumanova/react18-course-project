0e69a80df580d4cc2f6f001587e9d6920853dfb0
Andjelina Kumanova <angelinakumanova@gmail.com>
2025-04-22 12:10:24 +0300

    Refactoring: Create reusable API client

--- react18-course-project/game-hub/src/services/api-client.ts
+++ react18-course-project/game-hub/src/services/api-client.ts
@@ -1,15 +1,29 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: 'd215fc474eca4fcc8d7165890a4f94d2'
-    }
-})
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "d215fc474eca4fcc8d7165890a4f94d2",
+  },
+});
 
+class APIClient<T> {
+  endpoint: string;
 
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config?: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
