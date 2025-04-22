f9e5f54aa1ca4f7b3fa772d0f8d6db8037807e8b
Andjelina Kumanova <angelinakumanova@gmail.com>
2025-04-22 23:06:51 +0300

    Refactor: simplify time calculations

--- react18-course-project/game-hub/src/hooks/useGames.ts
+++ react18-course-project/game-hub/src/hooks/useGames.ts
@@ -2,6 +2,7 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient from "../services/api-client";
 import { Platform } from "./usePlatforms";
+import ms from "ms";
 
 export interface Game {
   id: number;
@@ -30,7 +31,7 @@ const useGames = (gameQuery: GameQuery) =>
     initialPageParam: 1,
     getNextPageParam: (lastPage, allPages) =>
       lastPage.next ? allPages.length + 1 : undefined,
-    staleTime: 6 * 60 * 60 * 1000, // 6h
+    staleTime: ms('6h')
   });
 
 export default useGames;
