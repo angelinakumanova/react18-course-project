0aa481d7c0daad27cf9d8456cea3cb98c6e5cda4
Andjelina Kumanova <angelinakumanova@gmail.com>
2025-04-26 12:44:42 +0300

    Fix layout

--- react18-course-project/game-hub/src/components/NavBar.tsx
+++ react18-course-project/game-hub/src/components/NavBar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/logo.webp'
 import ColorModeSwitch from './ColorModeSwitch'
 import SearchInput from './SearchInput'
+import { useNavigate } from 'react-router-dom'
 
 function NavBar() {
+  const navigate = useNavigate();
+
   return (
     <HStack padding="10px">
-        <Image src={logo} boxSize='60px' />
+        <Image src={logo} _hover={{cursor: 'pointer'}} boxSize='60px' onClick={() => navigate('/')} />
         <SearchInput />
         <ColorModeSwitch  />
     </HStack>
--- react18-course-project/game-hub/src/components/SearchInput.tsx
+++ react18-course-project/game-hub/src/components/SearchInput.tsx
@@ -2,23 +2,33 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store";
-
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
+  const navigate = useNavigate();
   const ref = useRef<HTMLInputElement>(null);
 
-  const setSearchText = useGameQueryStore(s => s.setSearchText);
+  const setSearchText = useGameQueryStore((s) => s.setSearchText);
 
   return (
-    <form onSubmit={(event) => {
-      event.preventDefault();
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
 
-      if (ref.current) setSearchText(ref.current.value)
-      
-    }}>
+        if (ref.current) {
+          setSearchText(ref.current.value);
+          navigate("/");
+        }
+      }}
+    >
       <InputGroup>
         <InputLeftElement children={<BsSearch />} />
-        <Input ref={ref} borderRadius={20} placeholder="Search games..." variant="filled" />
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder="Search games..."
+          variant="filled"
+        />
       </InputGroup>
     </form>
   );
