2413f165a48d2966c6ebe9da349eaa0cabcfe68e
Andjelina Kumanova <angelinakumanova@gmail.com>
2025-04-26 12:58:08 +0300

    Refactor entity interfaces

--- react18-course-project/game-hub/src/components/GameCard.tsx
+++ react18-course-project/game-hub/src/components/GameCard.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
-import { Game } from "../entities/Game";
+import  Game  from "../entities/Game";
 import Emoji from "./Emoji";
 import { Link } from "react-router-dom";
 
